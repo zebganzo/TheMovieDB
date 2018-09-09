0d0960b625fac0fe85200eabc4b2683a706f21ea
Sebastiano Catellani <sebastiano.catellani@futurice.com>
2018-09-09 13:27:25 +0200

    Project creation

---
+++ TheMovieDB/TheMovieDB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheMovieDB
+//
+//  Created by Sebastiano Catellani on 09.09.18.
+//  Copyright © 2018 Sebastiano Catellani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
