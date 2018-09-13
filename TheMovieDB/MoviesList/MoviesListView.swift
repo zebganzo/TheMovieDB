19f8edce67999530495ad714254a02db3089b626
Sebastiano Catellani <sebastiano.catellani@futurice.com>
2018-09-13 16:07:06 +0200

    Added table view.

--- TheMovieDB/MoviesList/MoviesListView.swift
+++ TheMovieDB/MoviesList/MoviesListView.swift
@@ -6,4 +6,26 @@
 //  Copyright © 2018 Sebastiano Catellani. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+final class MoviesListView: UIView {
+
+    let tableView = UITableView()
+
+    @available(*, unavailable)
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    init() {
+        super.init(frame: .zero)
+        self.backgroundColor = UIColor.white
+
+        self.addSubview(self.tableView)
+        self.tableView.snp.makeConstraints { make -> Void in
+            make.center.equalTo(self)
+            make.top.left.equalTo(self)
+        }
+    }
+}
