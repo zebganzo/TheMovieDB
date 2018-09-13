5e9a4292b7706e7e3a926357c036a089fc7f3b9f
Sebastiano Catellani <sebastiano.catellani@futurice.com>
2018-09-13 16:03:47 +0200

    Added Movie List classes.

---
+++ TheMovieDB/MoviesList/MoviesListView.swift
@@ -0,0 +1,9 @@
+//
+//  MoviesListView.swift
+//  TheMovieDB
+//
+//  Created by Sebastiano Catellani on 13.09.18.
+//  Copyright © 2018 Sebastiano Catellani. All rights reserved.
+//
+
+import Foundation
